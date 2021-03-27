88181ed33d356a39ddb3d103c01c5e519c686c72
julysky123 <51068333+julysky123@users.noreply.github.com>
2021-03-27 17:15:05 +0900

    iu 추가

---
+++ js/iu.js
@@ -0,0 +1,9 @@
+function zoomIn(event) {
+    event.target.style.width = "600px";
+    event.target.style.transition = "all 0.5s";
+  }
+
+function zoomOut(event) {
+    event.target.style.width = "500px";
+    event.target.style.transition = "all 0.5s";
+  }
